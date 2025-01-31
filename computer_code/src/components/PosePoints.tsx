cb16f880afd6a6df0b342ba4f74916a5319dafb5
Patrick O'Brien <paddysemail@gmail.com>
2025-01-31 14:19:28 +0000

    Show points on the feed

--- computer_code/src/components/PosePoints.tsx
+++ computer_code/src/components/PosePoints.tsx
@@ -1,16 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 
-// That's how you define the value of a pixel
-function drawPixel (canvasData, width, x, y, r, g, b, a) {
-    var index = (x + y * width) * 4;
-    
-    canvasData.data[index + 0] = r;
-    canvasData.data[index + 1] = g;
-    canvasData.data[index + 2] = b;
-    canvasData.data[index + 3] = a;
-}
-
-function numberOfAngles(point: [number, number][]){
+function numberOfAngles(point: Array<Array<number>>){
     let notNull = 0;
     point.forEach((cam) => {
         if (cam[0] != null) {
@@ -24,17 +14,15 @@ const WIDTH = 320;
 
 export default function PosePoints({numCams, points}:{numCams: number, points: Array<Array<Array<number>>>}){
   
-  const canvasRef = useRef<HTMLCanvasElement>()
+  const canvasRef = useRef<HTMLCanvasElement>(null)
   useEffect(() => {
     const canvas = canvasRef.current!;
     if (numCams > 0) {
         const context = canvas.getContext('2d')!;
         context.clearRect(0, 0, canvas.width, canvas.height)
         context.fillStyle =  "#00FF00";
-        console.log(points)
         points.forEach((point) => {
             let angles = numberOfAngles(point);
-            console.log(angles)
             if (angles == numCams) {
                 context.fillStyle =  "#00FF00";
             } else if (angles == numCams-1) {
