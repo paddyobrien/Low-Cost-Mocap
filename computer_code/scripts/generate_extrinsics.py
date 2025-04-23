7a4ccccb918e408e8bff222b1fe6fbc723bc506d
Patrick O'Brien <paddysemail@gmail.com>
2025-04-23 20:46:12 +0100

    Add a script to generate an initial guess on camera pose from physical data

---
+++ computer_code/scripts/generate_extrinsics.py
@@ -0,0 +1,46 @@
+from scipy.spatial.transform import Rotation
+import numpy as np
+import json
+import math
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super().default(obj)
+
+
+side = 0.3
+depth = 0.2
+half_side = side/2
+y_angle = 45
+x_angle = math.degrees(math.atan(depth/half_side)) * -1
+
+def create_rotation(x_angle, y_angle):
+    r = Rotation.from_euler("xyz", [x_angle, y_angle, 0], degrees=True)
+    return r.as_matrix()
+
+
+pose = [
+    {
+        "R": create_rotation(x_angle, y_angle),
+        "t": [-half_side, -side, -half_side]
+    },
+    {
+        "R": create_rotation(x_angle, -y_angle),
+        "t": [half_side, -side, -half_side]
+    },
+    {
+        "R": create_rotation(x_angle, y_angle - 180),
+        "t": [half_side, -side, half_side]
+    },
+    {
+        "R": create_rotation(x_angle, -y_angle - 180),
+        "t": [-half_side, -side, half_side]
+    },
+]
+
+print(json.dumps(pose, cls=NumpyEncoder))
+
+r = create_rotation(x_angle, y_angle)
+print(r.as_rotvec())
